441bfde7a0650093874ed4063ec1a4913505c074
Vaaiibhav Vaidya <vaaiibhav@live.com>
2020-04-07 18:59:31 +0530

    jquery post method

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 
 const express = require('express')
+var bodyParser = require('body-parser')
+
 const app = express()
 const port = 3000
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/users',(requ,resp) =>
 {
     
+
+})
+app.post('/login',urlencodedParser,(req,res) =>{
+    var reqobj = {};
+    reqobj = req.body;
+    console.log(reqobj);
+    res.send("Data Received Sucessfully");
+
 
 })
 
